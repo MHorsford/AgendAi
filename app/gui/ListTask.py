5c0dbad6ab09ed790fcb8f7c02772c4f6d9bbc08
Marcos Horsford <horsforddeveloper@gmail.com>
2024-05-29 10:52:55 -0300

    Added a search bar feature to filter the task list.

--- app/gui/ListTask.py
+++ app/gui/ListTask.py
@@ -8,9 +8,12 @@ class ListTask(ft.UserControl):
         super().__init__()
 
         self.tasks = TaskDAO().get_task()
-
+        self.colors = [ft.colors.BLUE, ft.colors.RED]
+        self.search = ft.TextField(label="Pesquisar", on_change=self.search_task)
+        self.datanotfound = ft.Text("Nenhuma Tarefa Encontrada")
         self.data_table = ft.DataTable(
-            border=ft.border.all(2, "#FFFFFF"),
+            width=1000,
+
             columns=[
                 ft.DataColumn(ft.Text("ID")),
                 ft.DataColumn(ft.Text("Nome")),
@@ -24,9 +27,22 @@ class ListTask(ft.UserControl):
             ]
         )
         self.load_task()
-        self.controls = [
-            self.data_table
-        ]
+
+    def build(self):
+        return ft.Column(
+            controls=[
+                ft.Container(
+                    content=self.search,
+                    padding=ft.padding.all(10),
+                    alignment=ft.alignment.center
+                ),
+                ft.Container(
+                    content=self.data_table,
+                    padding=ft.padding.all(10),
+                    alignment=ft.alignment.center
+                )
+            ]
+        )
 
     def load_task(self):
         for task in self.tasks:
@@ -43,4 +59,47 @@ class ListTask(ft.UserControl):
                 )
             )
 
+    def search_task(self, e):
+        search_name = self.search.value
+        list_task = list(filter(lambda _: search_name in _['Name'], self.tasks))
+        self.data_table.rows = []
+        if not self.search.value == "":
+            if len(list_task) > 0:
+                self.datanotfound.visible = False
+                for task in list_task:
+                    self.data_table.rows.append(
+                        ft.DataRow(
+                            cells=[
+                                ft.DataCell(ft.Text(str(task['ID']))),
+                                ft.DataCell(ft.Text(task['Name'])),
+                                ft.DataCell(ft.Text(task['Description'])),
+                                ft.DataCell(ft.Text(task['DateTime'])),
+                                ft.DataCell(ft.Text("Sim" if task['DalyAlarm'] else "Não")),
+                                ft.DataCell(ft.Text("Editar/Deletar"))
+                            ]
+                        )
+                    )
+                    self.update()
+            else:
+                self.datanotfound.visible = True
+                self.update()
+        else:
+            self.datanotfound.visible = False
+            for task in self.tasks:
+                self.data_table.rows.append(
+                    ft.DataRow(
+                        cells=[
+                            ft.DataCell(ft.Text(str(task['ID']))),
+                            ft.DataCell(ft.Text(task['Name'])),
+                            ft.DataCell(ft.Text(task['Description'])),
+                            ft.DataCell(ft.Text(task['DateTime'])),
+                            ft.DataCell(ft.Text("Sim" if task['DalyAlarm'] else "Não")),
+                            ft.DataCell(ft.Text("Editar/Deletar"))
+                        ]
+                    )
+                )
 
+            self.update()
+
+
+        pass
